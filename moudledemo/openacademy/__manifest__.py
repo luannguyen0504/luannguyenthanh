38ca47c84b4bfd7462f88d2899d9c2df5841d4b2
DTC1854802010293@ictu.edu.vn <dtc1854802010293@ictu.edu.vn>
2022-07-09 10:08:16 +0700

    first commit

---
+++ moudledemo/openacademy/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Open Academy",
+
+    'summary': """Manage trainings""",
+
+    'description': """
+        Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
+    """,
+
+    'author': "Luan Nguyen",
+    'website': "https://www.facebook.com/luannguyen.phoenixnguyen",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+
+    'data': [
+        # 'security/ir.model.access.csv',
+        'templates.xml',
+        'view / opencacdemy.xml' ,
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo.xml',
+    ],
+}
